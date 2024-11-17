e03e46357d9bf32784beb39e303af29287157484
CdeJohane <j.r.t.johane@gmail.com>
2024-11-17 21:16:04 +0200

    Added NAvbar Component

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <div className="navbar-header">
+                    <p className="navbar-brand"> Junior Johane</p>
+                </div>
+            </div>
+        </nav>
+    );
+}
