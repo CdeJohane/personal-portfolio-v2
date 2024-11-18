d1b29f0824442d6673bd8cb4942547bb9d969a58
CdeJohane <j.r.t.johane@gmail.com>
2024-11-18 11:28:02 +0200

    Added Social Media Links

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,10 +1,12 @@
 import './intro.css';
+import {SocialMedia} from "./social_media.jsx";
 
 export default function Intro() {
     return (
         <div className="intro">
             <h1>Hi, Im Junior</h1>
             <h2>I Speak In Code</h2>
+            < SocialMedia />
         </div>
     );
 }
---
+++ src/components/social_media.jsx
@@ -0,0 +1,11 @@
+import "./social_media.css"
+
+export const SocialMedia = () => {
+    return (
+        <div className="socialMedia">
+            <a className="skill-icons--github-light " href="https://github.com/CdeJohane" target="_blank" rel="noopener noreferrer"></a>
+            <a className="linkedin" href="https://www.linkedin.com/in/junior-johane-6b1185189/" target="_blank" rel="noopener noreferrer"></a>
+            <a className="email" href="https://mail.google.com/mail/?view=cm&fs=1&to=jurath.code@.com" target="_blank" rel="noopener noreferrer"></a>
+        </div>
+    );
+}
