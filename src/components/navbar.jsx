3e9dfac694e7020cc464e19595337f1151ce3f33
CdeJohane <j.r.t.johane@gmail.com>
2024-11-18 10:09:07 +0200

    Added basic navbar design

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,24 @@
+import './navbar.css';
+
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
-                <div className="navbar-header">
-                    <p className="navbar-brand"> Junior Johane</p>
-                </div>
+        <nav className="NavBar">
+            <div className="NavBar-center">
+                <ul className="nav-links">
+                    <li className="nav-home">
+                        <a href="/home">Home</a>
+                    </li>
+                    <li className="nav-about">
+                        <a href="/about">About</a>
+                    </li>
+                    <li className="nav-projects">
+                        <a href="/projects">Projects</a>
+                    </li>
+                    <li className="nav-contact">
+                        <a href="/contact">Contact</a>
+                    </li>
+
+                </ul>
             </div>
         </nav>
     );
