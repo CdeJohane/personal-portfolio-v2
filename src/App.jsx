35aa7e17082e1edff4cbbf7b5e6321eb0da7fec6
CdeJohane <j.r.t.johane@gmail.com>
2024-11-18 13:33:07 +0200

    Added About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from "./components/navbar.jsx";
 import Intro from "./components/intro.jsx";
+import About from "./components/about.jsx";
 
 function App() {
   return (
     <>
         <Navbar />
         <Intro  />
+        <About />
 
     </>
   )
---
+++ src/components/about.jsx
@@ -0,0 +1,24 @@
+import './about.css';
+
+export default function About(){
+    return (
+        <div className="about">
+            <h1>About Me</h1>
+            <p>
+                Hi, I&apos;m Junior, a recent graduate from the University Of Cape Town with a strong passion for
+                technology and data. <br/>
+                I&apos;m eager to see what the world has to offer in terms of code.
+            </p>
+            <p>
+                I did my undergraduate studies at the University Of Cape Town, studying Bachelor of Science,
+                specializing in Computer Science and Computer Engineering.<br/>
+                The main aspects of the degree, that I learnt form the Computer Science Side, was the basics of Python
+                and Java, Data Structures and algorithms, Software Engineering and Theory of Algorithms and
+                Computation, <br/>
+                whilst on the Computer Engineering Side, I learnt aspects of Electrical Engineering, as well as
+                programming STM boards and RaspberryPi Boards using C and C++, as well as Machine and Reinforcement
+                Learning with Python
+            </p>
+        </div>
+    );
+}
