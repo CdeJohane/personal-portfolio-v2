11d657ea0742d6a513df08d1e0d39eac28e9b75a
CdeJohane <j.r.t.johane@gmail.com>
2024-11-18 11:20:53 +0200

    Sizing of intro and NAvbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navbar from "./components/navbar.jsx";
+import Intro from "./components/intro.jsx";
 
 function App() {
   return (
     <>
         <Navbar />
+        <Intro  />
 
     </>
   )
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,7 +1,10 @@
+import './intro.css';
+
 export default function Intro() {
     return (
         <div className="intro">
             <h1>Hi, Im Junior</h1>
+            <h2>I Speak In Code</h2>
         </div>
     );
 }
