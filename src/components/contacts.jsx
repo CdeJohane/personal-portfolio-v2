1fe942f65b4104596e3e9b2c9b4bb1b455f7209d
CdeJohane <j.r.t.johane@gmail.com>
2024-11-19 12:30:05 +0200

    Added WebLinks to contact pages

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -5,9 +5,9 @@ export default function Contacts() {
         <div className="contacts">
             <h1>Contacts</h1>
             <div className="buttons">
-                <button className="emailBtn" type="button"> Email</button>
-                <button className="linkedinBtn" type="button"> LinkedIn</button>
-                <button className="githubBtn" type="button"> Github</button>
+                <button className="emailBtn" type="button" onClick={() => window.location.href = 'mailto:jurath.code@gmail.com'}> Email</button>
+                <button className="linkedinBtn" type="button" onClick={() => window.open('https://www.linkedin.com/in/junior-johane-6b1185189/', '_blank')}> LinkedIn</button>
+                <button className="githubBtn" type="button" onClick={() => window.open('https://github.com/CdeJohane', '_blank')}> Github</button>
             </div>
         </div>
     );
