275582ded62965a010f307e80893327b78385e9b
CdeJohane <j.r.t.johane@gmail.com>
2024-11-19 13:49:25 +0200

    Added further description

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -11,7 +11,7 @@ export default function Projects() {
                     <h2>Projects</h2>
                     <ProjectSummary weblink="" webpage="https://github.com/CdeJohane/soccer_prediction_admin" title="Prem Debates Prediction Admin" description="Developed in Flutter(Dart) and Flask(Python) this app was made as a way to track soccer predictions amongst friends that were in the game. This is specifically for the 2024/5 English Premier League Season"/>
                     <ProjectSummary weblink="" webpage="https://github.com/CdeJohane/PersonalPortfolio" title="Project Portfolio V1" description="This is my Initial Design of my Project Portfolio, created using React JS"/>
-                    <ProjectSummary weblink="" webpage="youtube.com" title="SolarViz App" description="Built using Flask(Python) and ReactJS(JavaScript)., this University Capstone project was built to maonitor energy usage at the University of Cape Town's Hasso-Plaatner D-School Building, in groups of 3"/>
+                    <ProjectSummary weblink="" webpage="youtube.com" title="SolarViz Web App" description="Built using Flask(Python) and ReactJS(JavaScript)., this University Capstone project was built to maonitor energy usage at the University of Cape Town's Hasso-Plaatner D-School Building, in groups of 3"/>
                 </div>
                 <div className="projects-sum-list">
                     <h2>Experience</h2>
