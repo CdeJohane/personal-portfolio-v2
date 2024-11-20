0a772bdf58ad3a5d502ef3d63416dfdad8ebb155
CdeJohane <j.r.t.johane@gmail.com>
2024-11-20 17:24:16 +0200

    Added Basic Screenshots for web page

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,7 @@
 import './projects.css';
 import ProjectSummary from "./project_summary.jsx";
 import Experience from "./experience.jsx";
+import screens from "../assets/screens/webpage.png";
 
 export default function Projects() {
     return (
@@ -9,9 +10,9 @@ export default function Projects() {
             <div className="projects">
                 <div className="projects-main">
                     <h2>Projects</h2>
-                    <ProjectSummary weblink="" webpage="https://github.com/CdeJohane/soccer_prediction_admin" title="Prem Debates Prediction Admin" description="Developed in Flutter(Dart) and Flask(Python) this app was made as a way to track soccer predictions amongst friends that were in the game. This is specifically for the 2024/5 English Premier League Season"/>
-                    <ProjectSummary weblink="" webpage="https://github.com/CdeJohane/PersonalPortfolio" title="Project Portfolio V1" description="This is my Initial Design of my Project Portfolio, created using React JS"/>
-                    <ProjectSummary weblink="" webpage="youtube.com" title="SolarViz Web App" description="Built using Flask(Python) and ReactJS(JavaScript)., this University Capstone project was built to maonitor energy usage at the University of Cape Town's Hasso-Plaatner D-School Building, in groups of 3"/>
+                    <ProjectSummary weblink={screens} webpage="https://github.com/CdeJohane/soccer_prediction_admin" title="Prem Debates Prediction Admin" description="Developed in Flutter(Dart) and Flask(Python) this app was made as a way to track soccer predictions amongst friends that were in the game. This is specifically for the 2024/5 English Premier League Season"/>
+                    <ProjectSummary weblink={screens} webpage="https://github.com/CdeJohane/PersonalPortfolio" title="Project Portfolio V1" description="This is my Initial Design of my Project Portfolio, created using React JS"/>
+                    <ProjectSummary weblink={screens} webpage="youtube.com" title="SolarViz Web App" description="Built using Flask(Python) and ReactJS(JavaScript)., this University Capstone project was built to maonitor energy usage at the University of Cape Town's Hasso-Plaatner D-School Building, in groups of 3"/>
                 </div>
                 <div className="projects-sum-list">
                     <h2>Experience</h2>
